d98068360117abdd44816ff2a7cd27c7ff157c9d
pkottev <112408964+pkottev@users.noreply.github.com>
2024-09-27 21:52:16 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,10 +7,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "bucket-ka"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "table-ka"
     encrypt        = true
   }
 }
